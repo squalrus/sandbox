9750c1d85eacafb017a4bf3bdd8a09c3467afc0e
Schulz <chad.schulz@it12-0235.it.corp>
2013-05-01 17:25:25 -0500

    Adds basic MongoDB implemementation.

--- mongo/server.js
+++ mongo/server.js
@@ -1,7 +1,14 @@
-var  express = require( 'express' )
-    ,app     = express();
+var express = require( 'express' )
+    ,mongo  = require( 'mongoskin' )
+    ,app    = express()
+    ,db
+    ,collection
+    ;
 
 app.listen( 1337, 'localhost' );
+
+db = mongo.db( 'localhost:27017/thing', { safe: true });
+collection = db.collection( 'stuff' );
 
 // Middleware
 app.use( express.bodyParser() );
@@ -11,7 +18,13 @@ app.set( 'view engine', 'ejs' );
 
 // Routing
 app.get( '/', function( req, res ){
-    res.render( 'index' );
+    collection.find().toArray( function( err, items ){
+        res.render( 'index', { data: items });
+    });
 });
+
+// AJAX route to find stuff?
+
+// Then one to send? ...to test?
 
 console.log( 'Server running at http://127.0.0.1:1337/' );
