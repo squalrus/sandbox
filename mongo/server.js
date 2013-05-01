83ae1e184e7d4ba4d5d154660a5d88796baff569
Schulz <chad.schulz@it12-0235.it.corp>
2013-05-01 17:03:26 -0500

    Basic server for mongo testing.

---
+++ mongo/server.js
@@ -0,0 +1,17 @@
+var  express = require( 'express' )
+    ,app     = express();
+
+app.listen( 1337, 'localhost' );
+
+// Middleware
+app.use( express.bodyParser() );
+
+// Application level settings
+app.set( 'view engine', 'ejs' );
+
+// Routing
+app.get( '/', function( req, res ){
+    res.render( 'index' );
+});
+
+console.log( 'Server running at http://127.0.0.1:1337/' );
