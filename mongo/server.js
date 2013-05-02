3ec14b3d3650f980f6fa613b767a07dea613b8cf
Schulz <chad.schulz@it12-0235.it.corp>
2013-05-02 15:12:19 -0500

    Adds master w/ worker processes to test load

--- mongo/server.js
+++ mongo/server.js
@@ -1,86 +1,92 @@
 var express  = require( 'express' )
     ,mongodb = require( 'mongodb' )
-    ,app     = express()
+    ,cluster = require( 'cluster' )
+    ,os      = require( 'os' )
     ,server
     ,db
     ,collection
     ;
 
-app.listen( 1337, 'localhost' );
+if( cluster.isMaster ){
 
-// Create server connection
-server = new mongodb.Server( 'localhost', '27017', {
-    auto_reconnect: true    // Default false
-    ,poolSize: 5            // Default 5
-});
+    // Fork workers
+    for( var i=0; i<os.cpus().length; i++ ){
+        cluster.fork();
+    }
+} else {
 
-// Connect/Create database
-db = new mongodb.Db( 'thing', server, {
-    safe: true
-});
+    var app = express();
 
-// Open database
-db.open( function( error, client ){
-    if( !error )
-        console.log( 'connected to db!' );
+    app.listen( 1337, 'localhost' );
 
-    // Connect/Create collection
-    db.createCollection( 'stuff'
-        ,{
-            capped: true
-            ,size: 1000
-            ,max: 10
-        }
-        ,function( error, collection ){
-            if( !error )
-                console.log( 'collection created!' );
+    // Create server connection
+    server = new mongodb.Server( 'localhost', '27017', {
+        auto_reconnect: true    // Default false
+        ,poolSize: 5            // Default 5
+    });
 
-            var cursor = collection.find({}, { tailable: true });
+    // Connect/Create database
+    db = new mongodb.Db( 'thing', server, {
+        safe: true
+    });
 
-            function tail( cursor ){
-
-                cursor.nextObject( function( error, item ){
-                    if( error )
-                        console.log( 'error' + error );
-                    if( item ){
-                        console.log( 'item: ' + JSON.stringify( item ) );
-                    }
-
-                    tail( cursor );
-                });
-            }
-
-            tail( cursor);
-
-        }
-    );
-});
-
-// Middleware
-app.use( express.bodyParser() );
-
-// Application level settings
-app.set( 'view engine', 'ejs' );
-
-// Routing
-app.get( '/', function( req, res ){
-    res.render( 'index' );
-});
-
-// AJAX route to find stuff?
-
-// Then one to send? ...to test?
-app.post( '/', function( req, res ){
+    // Open database
     db.open( function( error, client ){
-        var doc = { 'message': 'Hi there!', 'time': +new Date() };
+        if( !error )
+            console.log( 'connected to db!' );
+
+        // Connect/Create collection
+        db.createCollection( 'stuff'
+            ,{
+                capped: true
+                ,size: 1000
+                ,max: 10
+            }
+            ,function( error, collection ){
+                if( !error )
+                    console.log( 'collection created!' );
+
+                var cursor = collection.find({}, { tailable: true });
+
+                function tail( cursor ){
+
+                    cursor.nextObject( function( error, item ){
+                        if( error )
+                            console.log( 'error' + error );
+                        if( item ){
+                            console.log( 'item: ' + JSON.stringify( item ) );
+                        }
+
+                        tail( cursor );
+                    });
+                }
+
+                tail( cursor);
+
+            }
+        );
+    });
+
+    // Middleware
+    app.use( express.bodyParser() );
+
+    // Application level settings
+    app.set( 'view engine', 'ejs' );
+
+    // Routing
+    app.get( '/', function( req, res ){
+        res.render( 'index' );
+    });
+
+    app.post( '/', function( req, res ){
+        var doc = { 'message': req.body.message };
         db.collection( 'stuff' ).insert( doc, { w: 1 }, function( error, result ){
             console.log( 'Added record -- ' + JSON.stringify( result ) );
         });
+
+        res.send({ success: 1 });
     });
 
-    // db.close();
+    console.log( 'Server running at http://127.0.0.1:1337/' );
 
-    res.send({ success: 1 });
-});
-
-console.log( 'Server running at http://127.0.0.1:1337/' );
+}
