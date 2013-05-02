4294378ce47569623b2ea4cc25ae4a3c36566b53
Schulz <chad.schulz@it12-0235.it.corp>
2013-05-02 11:19:56 -0500

    Adds capped collection

--- mongo/server.js
+++ mongo/server.js
@@ -1,14 +1,59 @@
-var express = require( 'express' )
-    ,mongo  = require( 'mongoskin' )
-    ,app    = express()
+var express  = require( 'express' )
+    ,mongodb = require( 'mongodb' )
+    ,app     = express()
+    ,server
     ,db
     ,collection
     ;
 
 app.listen( 1337, 'localhost' );
 
-db = mongo.db( 'localhost:27017/thing', { safe: true });
-collection = db.collection( 'stuff' );
+// Create server connection
+server = new mongodb.Server( 'localhost', '27017', {
+    auto_reconnect: true    // Default false
+    ,poolSize: 5            // Default 5
+});
+
+// Connect/Create database
+db = new mongodb.Db( 'thing', server, {
+    safe: true
+});
+
+// Open database
+db.open( function( error, client ){
+    if( !error )
+        console.log( 'connected to db!' );
+
+    // Connect/Create collection
+    db.createCollection( 'stuff'
+        ,{
+            capped: true
+            ,size: 1000
+            ,max: 10
+        }
+        ,function( error, collection ){
+            if( !error )
+                console.log( 'collection created!' );
+
+            setInterval( function( ){
+                var doc = { 'message': 'Hi there!' };
+                collection.insert( doc );
+
+                var lastValue = mongodb.MinKey();
+                console.log( 'MinKey: ' + JSON.stringify( lastValue ) );
+            }, 5000 );
+
+            var lastValue = mongodb.MinKey();
+            console.log( 'MinKey: ' + JSON.stringify( lastValue ) );
+
+
+            function tail( ){
+                var lastValue = mongodb.MinKey();
+
+                console.log( 'MinKey: ' + lastValue );
+            }
+        });
+});
 
 // Middleware
 app.use( express.bodyParser() );
@@ -18,9 +63,7 @@ app.set( 'view engine', 'ejs' );
 
 // Routing
 app.get( '/', function( req, res ){
-    collection.find().toArray( function( err, items ){
-        res.render( 'index', { data: items });
-    });
+    res.render( 'index' );
 });
 
 // AJAX route to find stuff?
